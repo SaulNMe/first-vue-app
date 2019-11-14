02910532963322b474d453f08410c29f2ed81776
SaulNMe <amrs1399@gmail.com>
2019-11-14 13:21:34 -0600

    first-commit

---
+++ src/components/PrimaryBtn.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <button 
+            :disabled="disabled"
+            :class="['btn', (disabled ? 'disabled':'primary-btn')]"
+            v-on:click="addtoCount(1)"
+        >{{title}}</button>
+        <p>counter: {{counter}}</p>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from "vue-property-decorator";
+
+@Component
+export default class PrimaryBtn extends Vue {
+    @Prop() text!: string;
+    @Prop() disabled!: boolean;
+    @Prop() changeState!: Function;
+
+    counter = 0;
+    title= this.text;
+    
+    @Emit()
+    addtoCount(n: number) {
+        this.counter += n;
+        this.changeState();
+    }
+
+}
+// export default {
+//     props: {
+//         text: String
+//     },
+//     data(){
+//         return {
+//             title: 'Click me!',
+//             counter: 1
+//         }
+//     },
+//     methods: {
+//         addtoCount(n: number) {
+//             this.counter+=n;
+
+//         }
+//     }
+// }
+
+</script>
+<style scoped lang="scss">
+    .btn {
+        border-radius: 5px;
+         &:focus {
+            outline: none;
+        }
+    }
+    .primary-btn {
+        background-color: white;
+        color: #ddd;
+        border: 1px solid #42b983;
+        color: #42b983;
+        box-shadow: 0 0 5px 0 #808080;
+        &:hover {
+            background-color: #42b983;
+            color: white;
+        }
+    }
+    .disabled {
+        background-color: #808080;
+        color: white;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <PrimaryBtn 
+      v-bind:text="state" 
+      :changeState="changeState"
+      v-bind:disabled="disable"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App jojojo" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import PrimaryBtn from "@/components/PrimaryBtn.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    PrimaryBtn
+  },
+  data() {
+    return {
+      state: 'Click me!',
+      disable: false
+    }
+  },
+  methods: {
+    changeState() {
+      this.disable = true;
+    }
+  },
 };
 </script>
